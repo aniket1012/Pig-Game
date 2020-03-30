b84addc40b08b58b7fe5aba8a433893a1a17513e
Aniket <aniketupatel1@gmail.com>
2020-03-30 14:19:38 -0400

    added init function to remove active and winner class upon winner

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     
     //Check if player won the game 
     if(scores[activePlayer] >= 20) {
-        document.querySelector('#name-' + activePlayer).textContent = 'Winner'
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!'
         document.querySelector('.dice').style.display = 'none'
         document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
         document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
@@ -88,4 +88,15 @@ function init() {
     document.getElementById('score-1').textContent = '0'
     document.getElementById('current-0').textContent = '0'
     document.getElementById('current-1').textContent = '0'
+
+    document.getElementById('name-0').textContent = 'Player 1'
+    document.getElementById('name-1').textContent = 'Player 2'
+
+     document.querySelector('.player-0-panel').classList.remove('winner')
+     document.querySelector('.player-1-panel').classList.remove('winner')
+     document.querySelector('.player-0-panel').classList.remove('active')
+     document.querySelector('.player-1-panel').classList.remove('active')
+     document.querySelector('.player-0-panel').classList.add('active')
+
+
 }
